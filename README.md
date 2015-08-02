99503aafc6b160aac838eb1b8b49599fe6fadaaf
Caleb Gross <caleb.gross@gmail.com>
2015-08-02 16:27:42 -0500

    Rewrite script to reuse code

--- README.md
+++ README.md
@@ -1,21 +1,36 @@
 Macrophone
 ======
 
-After asking you for a song, Macrophone fetches lyrics from [MetroLyrics](http://www.metrylyrics.com) and reads it them in a voice of your choosing.
+After asking you for a song, Macrophone fetches lyrics from [MetroLyrics](http://www.metrylyrics.com) and reads them in a voice of your choosing.
 
+#### How It Works
+When Macrophone receives your preferred song and artist, it formats your selection and pastes it into a MetroLyrics URL. Once it grabs your song's lyrics page, it strips away all of the HTML to reveal the words to your song. That text is then sent to the OS X `say` command, a useful text-to-speech program bundled in with the system's default software.
+
+#### Usage
+Download the file and add execution permissions to the script. If you're unfamiliar with the command-line interface, you can open up the Terminal application on your computer and copy+paste the following commands:
+
+	cd ~/Downloads
+	curl -o macrophone.sh https://raw.githubusercontent.com/calebgross/macrophone/master/macrophone.sh
+	sudo chmod +x macrophone.sh
+	
+ Invoke the program by simply typing:
+ 
+	./macrophone.sh
+	
 When prompted to select a song, artist, or voice, enter your choice followed by the "Enter" key. Don't worry about capitalization—your input will be processed and formatted automatically. Note that some song names might be formatted in an odd way on the MetroLyrics site; if your selection doesn't work after a few tries, try checking their [website](http://www.metrylyrics.com) to see how they've listed the song.
 
-Your most recent selections are saved in `/tmp/.sing_config`. For example, simply hitting "Enter" after each of the following lines:
+Your most recent selections are saved in `/tmp/.macrophone_config`. For example, simply hitting "Enter" after each of the following lines:
 
-	Enter song [wrecking-ball]: 
-	Enter artist [miley-cyrus]: 
-	Enter voice (type help for a list of voices) [Alex]:
+	Enter song [wrecking ball]: 
+	Enter artist [miley cyrus]: 
+	Enter voice (for a list of voices, type help) [alex]:
 	
-will tell your computer to use those settings. You should see:
+will tell your computer to use those settings. You should then see:
 
-	Now singing wrecking-ball by miley-cyrus in the voice of Alex (hit ctrl+c to cancel)...
+	Now singing wrecking ball by miley cyrus in the voice of alex (hit ctrl+c to cancel)...
 	
 #### To-do
-* Display most recent configurations in a more easily readable format
-* Reuse code, and generally rewrite it to be more efficient
-* Allow user to select voices with spaces in the name
+* Search across multiple lyrics sites
+* Allow user to optionally pass song/artist/voice selections as arguments to the executable at invocation, enabling selections to be piped in from a playlist file
+* Print lyrics as they're being read (probably unrealistic, but fun to suggest)
+* Implement error handling on user input
--- macrophone.sh
+++ macrophone.sh
@@ -1,48 +1,89 @@
 #!/bin/bash
+# Description:  A program to tell your Mac to read (i.e. "sing") a song of your choosing.
+# Author:		Caleb Gross
+# Note:			Used a custom associative array implementation since Bash v3 doesn't have
+#				one by default, and it appears that's what Macs are shipped with.
+#				KEY=${ARRAY%%=*}
+#	    		VALUE=${ARRAY#*=}
 
-CONFIG="/tmp/.sing_config"
-if [ ! -f $CONFIG ]; then
- echo 'LAST_S=' >> $CONFIG
- echo 'LAST_A=' >> $CONFIG
- echo 'LAST_V=' >> $CONFIG
-fi
+# set config file location and settings parameters
+CONFIG="/tmp/.macrophone_config"
+SETTINGS=(
+	"SONG="
+	"ARTIST="
+	"VOICE="
+	)
 
-LAST_S=$(cat $CONFIG | grep "LAST_S=" | sed -E 's/LAST_S=//')
-LAST_A=$(cat $CONFIG | grep "LAST_A=" | sed -E 's/LAST_A=//')
-LAST_V=$(cat $CONFIG | grep "LAST_V=" | sed -E 's/LAST_V=//')
+# wrapper for retrieving settings
+# $1 : type 	 (song|artist|voice)	
+# $2 : formatted (optional)
+get() {
 
-echo -n "Enter song [$LAST_S]: "
-read s
-if [[ $s = "" ]]; then
- song=$LAST_S
+	if 	 [ $1 = song ];   then INDEX=0;
+	elif [ $1 = artist ]; then INDEX=1;
+	elif [ $1 = voice ];  then INDEX=2;
+	fi
+
+	if [[ "$#" -gt 1 && $2 = "formatted" && $1 != voice ]]; then
+		echo ${SETTINGS[$INDEX]#*=} | sed 's/\ /-/g'
+	else
+		echo ${SETTINGS[$INDEX]#*=}
+	fi
+	
+}
+
+# read settings from config file if it already exists
+# otherwise, create a new blank config file
+if [ -f $CONFIG ]
+	then
+	for (( i=0; i<${#SETTINGS[@]}; i++ ))
+	do
+		LINE=$( grep "${SETTINGS[i]}" $CONFIG )
+		VALUE=${LINE#*=}
+		SETTINGS[i]=${SETTINGS[i]}$VALUE
+	done
 else
- song=$(echo $s | tr '[:upper:]' '[:lower:]' | sed 's/\ /-/g')
- sed -iE 's/LAST_S=.*/LAST_S='$song'/' $CONFIG
+	printf '%s\n' "${SETTINGS[@]}" > $CONFIG
 fi
 
-echo -n "Enter artist [$LAST_A]: "
-read a
-if [[ $a = "" ]]; then
- artist=$LAST_A
-else
- artist=$(echo $a | tr '[:upper:]' '[:lower:]' | sed 's/\ /-/g')
- sed -iE 's/LAST_A=.*/LAST_A='$artist'/' $CONFIG
-fi
+# prompts to be displayed to user
+PROMPTS=(
+	"SONG&Enter song [$(get song)]: "
+	"ARTIST&Enter artist [$(get artist)]: "
+	"VOICE&Enter voice (for a list of voices, type help) [$(get voice)]: "
+	)
 
-echo -n "Enter voice (type help for a list of voices) [$LAST_V]: "
- read v
-while [[ $v = "help" ]]; do
- say -v ?
- echo -n "Enter voice (type help for a list of voices) [$LAST_V]: "
- read v
+# prompt user for input
+# if they enter a new selection, process it
+# otherwise, use selection from config file
+for (( i=0; i<${#PROMPTS[@]}; i++ ))
+do
+	TYPE=${PROMPTS[i]%%&*}
+	PROMPT=${PROMPTS[i]#*&}
+	echo -n "$PROMPT "
+	read INPUT
+	while [[ $TYPE = "VOICE" && $INPUT = "help" ]]; do
+		say -v ?
+		echo -n "$PROMPT "
+		read INPUT
+	done
+	if [[ $INPUT != "" ]]
+		then
+		SELECTION=$(echo $INPUT | tr '[:upper:]' '[:lower:]')
+		SETTINGS[i]=$TYPE=$SELECTION
+	fi
 done
-if [[ $v = "" ]]; then
- voice=$LAST_V
-else
- voice=$v
- sed -iE 's/LAST_V=.*/LAST_V='$v'/' $CONFIG
-fi
 
-echo "Now singing $song by $artist in the voice of $voice (hit ctrl+c to cancel)..."
-# curl -s http://www.songlyrics.com/$artist/$song-lyrics/ | tr -d '\r\n' | grep -oE '<p id="songLyricsDiv".*?>.*?</p>' | perl -pe 's|<br />| |g' | perl -pe 's|<.*?>||g' | say -v $voice
-curl -s http://www.metrolyrics.com/$song-lyrics-$artist.html | tr -d '\r\n' | grep -oE '<div id="lyrics-body-text">.*?</div>' | perl -pe 's|<br />| |g' | perl -pe 's|<.*?>||g' | say -v $voice
+# save current selections
+printf '%s\n' "${SETTINGS[@]}" > $CONFIG
+
+# print feedback to user
+echo "Now singing $(get song) by $(get artist) in the voice of $(get voice) (hit ctrl+c to cancel)..."
+
+# sing selected song
+curl -s http://www.metrolyrics.com/$(get song formatted)-lyrics-$(get artist formatted).html \
+| tr -d '\r\n' \
+| grep -oE '<div id="lyrics-body-text">.*?</div>' \
+| perl -pe 's|<br />| |g' \
+| perl -pe 's|<.*?>||g' \
+| say -v "$(get voice)"
